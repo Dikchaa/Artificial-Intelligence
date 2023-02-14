621a9dbc2391dd96e32587d4503657272a500e6b
Dikcha Singh <101415059+Dikchaa@users.noreply.github.com>
2023-02-14 19:02:27 +0530

    Create BFS+DFS.py

---
+++ BFS+DFS.py
@@ -0,0 +1,44 @@
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+visit = set()
+
+visited = [] 
+queue = []     
+
+def bfs(visited, graph, node):
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    m = queue.pop(0) 
+    print (m, end = " ") 
+
+    for n in graph[m]:
+      if n not in visited:
+        visited.append(n)
+        queue.append(n)
+
+def dfs(visit, graph, node):  
+    if node not in visit:
+        print (node,end=" ")
+        visit.add(node)
+        for n in graph[node]:
+            dfs(visit, graph, n)
+
+print("BFS is:")
+bfs(visited, graph, '5') 
+print('\n')
+print("DFS is:")
+dfs(visit, graph, '5')
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
